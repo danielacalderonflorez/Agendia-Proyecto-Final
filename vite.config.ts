ad1ce148c13a5e1dffd7ab741b14bbd3c74516fe
danielacalderonflorez <daniela.calderon.856@gmail.com>
2025-11-21 01:28:54 -0500

    Configurar para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Agendia-Proyecto-Final/', // 👈 Agrega esta línea
   server: {
     host: "::",
     port: 8080,
